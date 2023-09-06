96b24ab4fd53c5e358ea1856604ed41158c05223
habibullo <azimovhabibullo1@gmail.com>
2023-09-06 20:00:42 +0500

    rest framework's get and post added

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import all, detail, all2, detail2
+from .views import all, detail, all2, detail2, ListAppView, DetailAppView, CreateAppView
 
 urlpatterns = [
     path('detail/<int:android_id>', detail),
     path('all/', all),
     path('detail2/<int:ios_id>', detail2),
     path('all2/', all2),
+    path('listApps/', ListAppView.as_view()),
+    path('detailAppView/<int:android_id>', DetailAppView.as_view()),
+    path('create/',CreateAppView.as_view()),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import AppIOS, AppAndroid
 from django.http import JsonResponse
 from .serializers import AppIOsSerializer, AppAndroidSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
 
-def detail(request, android_id):
+def detail(request, android_id): 
     apps = get_object_or_404(AppAndroid, id = android_id)
     apps_data = AppAndroidSerializer(apps)
     
@@ -29,3 +30,25 @@ def all2(request):
     res = AppIOsSerializer(a, many=True)
     
     return JsonResponse(res.data, safe=False)      
+
+class ListAppView(APIView):
+    def get(self, request):
+        all_data = AppAndroid.objects.all()
+        result = AppAndroidSerializer(all_data, many=True)
+        return Response(result.data)   
+
+class DetailAppView(APIView):
+    def get(self, request, *args, **kwargs):
+        app = get_object_or_404(AppAndroid, id = kwargs["android_id"])
+        result = AppAndroidSerializer(app)
+
+        return Response(result.data)
+
+class CreateAppView(APIView):
+    def post(self, request):
+        user_body = request.data
+        serializer = AppAndroidSerializer(data=user_body)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)     
+        return Response(serializer.errors)      
